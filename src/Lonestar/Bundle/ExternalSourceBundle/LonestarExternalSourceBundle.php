7e5c2cb1904652487ebcfdbec4d346112b113fc0
Daniel Cousineau <dcousineau@gmail.com>
2014-10-15 23:53:03 -0400

    Added mock bundle and fabfile to prep for deployment

---
+++ src/Lonestar/Bundle/ExternalSourceBundle/LonestarExternalSourceBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lonestar\Bundle\ExternalSourceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class LonestarExternalSourceBundle
+ * @package Lonestar\Bundle\ExternalSourceBundle
+ */
+class LonestarExternalSourceBundle extends Bundle
+{
+}
